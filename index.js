a10b99a4c06759cf17b0f495485773e8c8a0be5f
Sakib0017 <sakibkamal0017@gmail.com>
2024-08-06 15:26:15 +0600

    Get

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
     res.send("Hello, World")
 })
+app.get("/sakib", (req, res) => {
+    res.send("Hello, Sakib")
+})
 
 app.listen(port, () =>{
     console.log(`Server is running on port ${port}`);
