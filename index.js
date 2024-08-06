2f5c02053348b21839c9e95d027ba808c8150e07
Sakib0017 <sakibkamal0017@gmail.com>
2024-08-06 15:20:51 +0600

    Get

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-import express from "express";
-const app =express();
-const port = process.env.PORT || 3000;
-app.get("/", (req, res) => {
-    res.send("Hello, World")
-})
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+const app = express();
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+mongoose.connect('mongodb+srv://admin-sakib:sakib124000@cluster0.x55l3gi.mongodb.net/test');
 
-app.listen(port, () =>{
-    console.log(`Server is running on port ${port}`);
+const itemSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+app.get('/items', async (req, res) => {
+    try {
+      const items = await Item.find();
+      res.json(items);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+ 
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
