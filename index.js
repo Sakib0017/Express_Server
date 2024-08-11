2b623218ffa075fbc9ce55921be297dbdaadf9ed
Sakib0017 <sakibkamal0017@gmail.com>
2024-08-11 12:50:24 +0600

    Database Connect

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-import express from "express";
+const express = require("express");
 const app =express();
 const port = process.env.PORT || 3000;
+const connectDb = require("./utils/db")
 app.get("/", (req, res) => {
     res.send("Hello, World")
 })
 app.get("/sakib", (req, res) => {
     res.send("Hello, Sakib")
 })
-
-app.listen(port, () =>{
-    console.log(`Server is running on port ${port}`);
-});
+connectDb().then(() => {
+    app.listen(port, () =>{
+        console.log(`Server is running on port ${port}`);
+    });
+})
---
+++ utils/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const URI = "mongodb+srv://sakib-admin:sakib124000@cluster0.x55l3gi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/test1";
+
+const connectDb = async () =>{
+    try {
+        await mongoose.connect(URI);
+        console.log('connection Successfull')
+    } catch (error) {
+        console.error("Database connection failed");
+        process.exit(0);
+    }
+}
+module.exports = connectDb;
